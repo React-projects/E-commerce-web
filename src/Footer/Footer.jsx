ed70ac11dbb13ca3ec3a105431d9beba7c169dd8
BAssal <baslm8496@gmail.com>
2024-08-28 21:58:35 +0300

    creating and styling footer section

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Footer.css";
+import FooterLogo from "../Components/Assets/logo_big.png";
+import InstgramIcon from "../Components/Assets/instagram_icon.png";
+import PintesterIcon from "../Components/Assets/pintester_icon.png";
+import WhatsappIcon from "../Components/Assets/whatsapp_icon.png";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-logo">
+        <img src={FooterLogo} alt="footer-logo" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="footer-links">
+        <li>Company</li>
+        <li> Products</li>
+        <li>Officer</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <div className="footer-socials">
+        <div className="footer-icon-container">
+          <img src={InstgramIcon} alt="instagram-Icon" />
+        </div>
+        <div className="footer-icon-container">
+          <img src={PintesterIcon} alt="pintester-Icon" />
+        </div>
+        <div className="footer-icon-container">
+          <img src={WhatsappIcon} alt="whatsapp-Icon" />
+        </div>
+      </div>
+			<div className="footer-copyright">
+				<hr />
+				<p>Copyright @ 2024 - ALL Rights Reserved . </p>
+			</div>
+    </div>
+  );
+}
+
+export default Footer;
