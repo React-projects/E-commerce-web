67b147fe16e1bd41ae9ef30349a8412254d1891f
BAssal <baslm8496@gmail.com>
2024-09-01 14:24:21 +0300

    editing some css in newsletter section

--- src/Components/NewLetter/NewLetter.jsx
+++ src/Components/NewLetter/NewLetter.jsx
@@ -6,7 +6,7 @@ function NewLetter() {
       <h1>Get Exclusive Offer on Your Email</h1>
       <p>Subscribe to NewLetter and Stay Update</p>
       <div className="subscribe-input">
-        <input type="email" placeholder="Your Email ..." />
+        <input type="email" placeholder="Your Email ..."   aria-label="Close" />
         <button>Subscribe</button>
       </div>
     </div>
